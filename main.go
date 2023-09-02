21efccfc6963a6a8dea833b4db86c4c5cfd0f6c3
Harsha Vardhan <harshavardhanchrs@gmail.com>
2023-09-03 03:10:08 +0530

    Adds store event data in redis
    - Also adds helper redis structures for event consumer

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
 	"log"
 	"net/http"
+	"time"
 )
+
+var redisClient *redis.Client
+
+// constant list of possible destinations, move to constants/config later
+var destinations = []string{"A", "B", "C", "D"}
 
 type event struct {
 	UserID  string `json:"userId"`
@@ -13,6 +22,15 @@ type event struct {
 
 type eventResponse struct {
 	Msg string `json:"msg"`
+}
+
+type eventMetadata struct {
+	// duplicating event fields to avoid embedded structs reformat before write to redis
+	UserID        string `redis:"userId"`
+	Payload       string `redis:"payload"`
+	Timestamp     int64  `redis:"timestamp"`     // (to be generally set by the client) set to server time if not sent (unix epoch nanoseconds)
+	ExecTimestamp int64  `redis:"execTimestamp"` // initially equal to timestamp, on retries or waits set to next exec timestamp
+	RetryCount    int    `redis:"retryCount"`
 }
 
 func parseRequest(req *http.Request) (ev event) {
@@ -31,13 +49,50 @@ func sendResponse(w http.ResponseWriter, msg string) {
 	_ = json.NewEncoder(w).Encode(resp)
 }
 
+func storeEvent(ev event) {
+	var em = eventMetadata{
+		Timestamp: time.Now().UnixNano(),
+		UserID:    ev.UserID,
+		Payload:   ev.Payload,
+	}
+	em.ExecTimestamp = em.Timestamp
+
+	// Add event metadata to redis
+	id := uuid.New().String()
+	log.Println(id)
+	ctx := context.Background()
+	// need to handle redis errors later
+	// Store event data mapped to id in a hash
+	redisClient.HSet(ctx, id, em)
+
+	// Add broadcast to destinations
+	for _, destination := range destinations {
+		// Each destination has a sorted set from which events are picked up by earliest time first
+		redisClient.ZAdd(ctx, destination, redis.Z{
+			Score:  float64(em.ExecTimestamp),
+			Member: id,
+		})
+
+		// Each destination has a list for order in which events have to be processed
+		redisClient.LPush(ctx, destination, id)
+	}
+}
+
 func receiveEvent(w http.ResponseWriter, req *http.Request) {
-	parseRequest(req)
+	ev := parseRequest(req)
+	storeEvent(ev)
 	sendResponse(w, "Event received")
 }
 
 func main() {
 	http.HandleFunc("/receive_event", receiveEvent)
+
+	redisAddr := "localhost:6379"
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: "",
+		DB:       0,
+	})
 
 	port := "8090"
 	err := http.ListenAndServe(":"+port, nil)
